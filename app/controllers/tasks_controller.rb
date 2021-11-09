6450d3e2095bb56b7ab84e36c42106bc6ed2df1b
kenneth campbell <kencam92@gmail.com>
2021-11-09 16:54:18 +0000

    didnt finish tasks to do active record thing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,17 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def edit
+    @task = task.find(params[:id])
+  end
+
+  def create
+    @task = task.new(params[:task_params])
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def update; end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/task/:id', to: 'task#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
